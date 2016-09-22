f2d6c592a8838056073ab7a9b6ea89cb96a25a7f
Sachin Ranchod <sachin.ranchod@gmail.com>
2016-09-21 17:45:51 -0700

    add datastore

--- app.rb
+++ app.rb
@@ -3,6 +3,22 @@ require 'sinatra'
 require 'json'
 require 'chunky_png'
 
+
+before do
+  @teams ||= load_data
+
+  begin
+    request.body.rewind
+    data = JSON.parse request.body.read
+    if data["team_id"]
+      @team = find_team(@teams, data["team_id"], data["event"]["user"])
+    end
+  rescue JSON::ParserError
+    p "No JSON in body"
+  end
+
+end
+
 get '/' do
   erb :index
 end
@@ -17,12 +33,15 @@ get '/oauth' do
     }
 
     res = RestClient.post 'https://slack.com/api/oauth.access', options, content_type: :json
+    save(JSON.parse(res))
 
-    p res
+    @teams.to_s
   end
 end
 
 post '/events' do
+
+  request.body.rewind
   data = JSON.parse(request.body.read, object_class: OpenStruct)
 
   case data.type
@@ -33,43 +52,87 @@ post '/events' do
   when "event_callback"
     event = data.event
     if event.subtype && event.subtype == "file_share"
-      file = event.file
-      upload(file, fetch_and_compose_png(file)) if file.filetype == "png"
+      if @team && event.user != @team.bot["bot_user_id"]
+        file = event.file
+        unless already_processing?(file.timestamp)
+          fetch_and_compose_png(file)
+          upload(file, event.channel) if file.filetype == "png"
+        end
+      end
     end
   end
+
+  return 200
 end
 
 def fetch_and_compose_png(file)
-  filename = fetch_png(file.url_private)
-  compose_png(filename)
+  p "file_and_compose_png"
+  File.open("./tmp/#{file.timestamp}", 'wb') do |f|
+    f << fetch_png(file.url_private)
+  end
 
-  filename
+  compose_png(file.timestamp)
 end
 
 def fetch_png(url)
-  res = RestClient.get(url, {"Authorization" => "Bearer #{ENV['BOT_ACCESS_TOKEN']}" })
-  filename = "#{(Time.now.to_f * 1000).to_i}.png"
+  p "fetch_png"
+  res = RestClient.get(url, {"Authorization" => "Bearer #{@team.access_token}" })
   if res.code == 200
-    File.open("./tmp/#{filename}", 'wb'){ |file| file << res.body }
+    return res.body
+  else
+    raise 'Download failed'
   end
-
-  return filename
 end
 
 def compose_png(filename)
+  p "compose_png"
   avatar = ChunkyPNG::Image.from_file("./tmp/#{filename}")
   badge  = ChunkyPNG::Image.from_file('./files/overlay.png')
   avatar.compose!(badge, 100, 100)
   avatar.save("./tmp/composed/#{filename}", :fast_rgba) # Force the fast saving routine.
 end
 
-def upload(file, new_filename)
+def upload(file, channel)
+  p "upload"
   options = {
-    token: ENV['BOT_ACCESS_TOKEN'],
-    file: File.new("./tmp/composed/#{new_filename}", 'rb'),
-    filename: file.name + "_composed",
-    title: file.title + "_composed"
+    token: @team.bot["bot_access_token"],
+    file: File.new("./tmp/composed/#{file.timestamp}", 'rb'),
+    filename: "composed_" + file.name,
+      title: "Composed " + file.title,
+      channels: channel
   }
 
   res = RestClient.post 'https://slack.com/api/files.upload', options, content_type: :json
+  p res.body
+end
+
+def already_processing?(filename)
+  return true if FileTest.exist?("./tmp/#{filename}")
+  File.open("./tmp/#{filename}", "w") {}
+
+  false
+end
+
+def save(team)
+  @teams << team
+
+  File.open("store.json","w") do |file|
+    file.write @teams.to_json
+  end
+end
+
+def load_data
+  if FileTest.exist?("store.json")
+    JSON.parse(File.read('store.json'))
+  else
+    []
+  end
+end
+
+def find_team(teams, team_id, user_id)
+  all_matching_auths = teams.select {|t| t["team_id"] == team_id }
+  user_auth = all_matching_auths.detect {|t| t["user_id"] == user_id }
+  team = user_auth || all_matching_auths.first
+
+  team ? OpenStruct.new(team) : nil
 end
